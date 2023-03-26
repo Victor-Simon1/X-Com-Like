b52f0f2782ae9bc836699ecfba34db2f3f266827
AlexandreFlanquart <104308588+AlexandreFlanquart@users.noreply.github.com>
2023-03-26 17:53:14 +0200

    :construction: Add scripts

---
+++ Source/XComLike/Private/ItemHpBehavior.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ItemHpBehavior.h"
+
+
+UItemHpBehavior::UItemHpBehavior() {
+	//Super();
+}
+
+void UItemHpBehavior::ConsumeItem(AActor* Player) {
+	//UItemBehavior::ConsumeItem(Player);
+}
+
---
+++ Source/XComLike/Public/ItemBehavior.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ItemBehavior.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class XCOMLIKE_API UItemBehavior : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UItemBehavior();
+
+	void ConsumeItem(AActor* Player);
+	
+};
+
